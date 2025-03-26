e51fece1c48a284620ff8ec3ee1947a75a7d8450
Niklas Palm <niklas-palm@hotmail.com>
2025-03-26 22:57:11 +0100

    Add utils package initialization

---
+++ backend/lambda/utils/__init__.py
@@ -0,0 +1,6 @@
+"""EQT Portfolio Research Utils Package"""
+
+from aws_lambda_powertools import Logger
+
+# Initialize shared logger for all modules
+logger = Logger(service="eqt-portfolio-research")
